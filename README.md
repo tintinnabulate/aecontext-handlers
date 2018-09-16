d3ced41ae59e4dea32cc2d53404d72603dea3ed4
Tintinnabulate <tintinn@abulate>
2018-09-17 00:18:15 +0100

    More tweaking

--- README.md
+++ README.md
@@ -7,4 +7,88 @@ aecontext-handlers are a bunch of AppEngine context handlers that wrap the stand
 
 ## Example usage
 
-Foo
+foo.go:
+
+```go
+package main
+
+import (
+    "encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/csrf"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
+	"github.com/spf13/viper"
+	"github.com/stripe/stripe-go"
+	stripeClient "github.com/stripe/stripe-go/client"
+
+	"golang.org/x/net/context"
+
+	"google.golang.org/appengine/urlfetch"
+)
+
+// createHTTPRouter : create a HTTP router where each handler is wrapped by a given context
+func createHTTPRouter(f handlers.ToHandlerHOF) *mux.Router {
+	appRouter := mux.NewRouter()
+	appRouter.HandleFunc("/signup", f(getSignupHandler)).Methods("GET")
+	return appRouter
+}
+
+// getSignupHandler : show the signup form (SignupURL)
+func getSignupHandler(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+}
+
+func init() {
+	router := createHTTPRouter(handlers.ToHTTPHandlerConverter)
+	http.Handle("/", csrfProtectedRouter)
+}
+```
+
+foo_test.go:
+
+```go
+package main
+
+import (
+    "fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+
+	c "github.com/smartystreets/goconvey/convey"
+	"github.com/spf13/viper"
+
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/aetest"
+)
+
+// TestGetSignupPage does just that
+func TestGetSignupPage(t *testing.T) {
+	ctx, inst := handlers.GetTestingContext()
+	defer inst.Close()
+
+	c.Convey("When visit the signup page", t, func() {
+		r := createHTTPRouter(handlers.ToHTTPHandler(ctx))
+		record := httptest.NewRecorder()
+
+		req, err := http.NewRequest("GET", "/signup", nil) // URL-encoded payload
+
+		c.So(err, c.ShouldBeNil)
+
+		c.Convey("The next page body should contain \"Please enter your email address\"", func() {
+			r.ServeHTTP(record, req)
+			c.So(record.Code, c.ShouldEqual, http.StatusOK)
+		})
+	})
+}
+```
+
--- aecontext-handlers.go
+++ aecontext-handlers.go
@@ -5,34 +5,39 @@ import (
 
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/aetest"
 )
 
-// Standard http handler
-type handlerFunc func(w http.ResponseWriter, r *http.Request)
+// HandlerFunc : the standard http handler
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
 
-// Our context.Context http handler
-type contextHandlerFunc func(ctx context.Context, w http.ResponseWriter, r *http.Request)
+// ContextHandlerFunc : like HandlerFunc, plus the golang.org/x/net/context Context
+type ContextHandlerFunc func(ctx context.Context, w http.ResponseWriter, r *http.Request)
 
+// ToHandlerHOF :
 // Higher order function for changing a HandlerFunc to a ContextHandlerFunc,
 // usually creating the context.Context along the way.
-type ContextHandlerToHandlerHOF func(f contextHandlerFunc) handlerFunc
+type ToHandlerHOF func(f ContextHandlerFunc) HandlerFunc
 
-// Creates a new Context and uses it when calling f
-func ContextHandlerToHTTPHandler(f contextHandlerFunc) handlerFunc {
+// ToHTTPHandler : returns a HandlerFunc which uses a new AppEngine context inside it
+func ToHTTPHandler(f ContextHandlerFunc) HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := appengine.NewContext(r)
 		f(ctx, w, r)
 	}
 }
 
-func ConvertTestingContextHandlerToHTTPHandler(ctx context.Context) ContextHandlerToHandlerHOF {
-	return func(f contextHandlerFunc) handlerFunc {
+// ToHTTPHandlerConverter : returns a higher order function that converts
+// a testing context handler to a standard HTTP handler
+func ToHTTPHandlerConverter(ctx context.Context) ToHandlerHOF {
+	return func(f ContextHandlerFunc) HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			f(ctx, w, r)
 		}
 	}
 }
 
+// GetTestingContext : gets appengine context and appengine test instance
 func GetTestingContext() (context.Context, aetest.Instance) {
 	inst, _ := aetest.NewInstance(
 		// TODO: pass these aetest.Options in as param to getTestingContext
