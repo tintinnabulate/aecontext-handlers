7339e832d16cb1f93c382b9db3d9eb8480316dba
Tintinnabulate <tintinn@abulate>
2018-09-25 11:58:04 +0100

    We need to move GetTestingContext into its own package to overcome a restriction of GoogleAppEngine not having aetest in its standard environment.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -38,21 +38,3 @@ func ToHTTPHandlerConverter(ctx context.Context) ToHandlerHOF {
 		}
 	}
 }
-
-// GetTestingContext : gets an aetest.Context aetest.Instance to pass into our tests.
-// Always call inst.Close() when you are done with it, and at least at the end of each test.
-// Use ToHTTPHandlerConverter to convert the aetest.Context to our normal AppEngine Context.
-func GetTestingContext() (context.Context, aetest.Instance) {
-	inst, _ := aetest.NewInstance(
-		// TODO: pass these aetest.Options in as param to GetTestingContext
-		&aetest.Options{
-			SuppressDevAppServerLog:     true,
-			StronglyConsistentDatastore: true,
-		})
-	req, err := inst.NewRequest("GET", "/", nil)
-	if err != nil {
-		inst.Close()
-	}
-	ctx := appengine.NewContext(req)
-	return ctx, inst
-}
